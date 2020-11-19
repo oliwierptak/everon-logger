c8a9cdb6dd0fe636a7d9c80d908e98fd9fa74470
Oliwier Ptak <oliwierptak@gmail.com>
2020-11-19 19:00:17 +0100

    specification

--- src/Logger/Contract/Plugin/PluginFactoryInterface.php
+++ src/Logger/Contract/Plugin/PluginFactoryInterface.php
@@ -6,5 +6,14 @@ use Everon\Logger\Configurator\AbstractPluginConfigurator;
 
 interface PluginFactoryInterface
 {
+    /**
+     * Specification:
+     * - Create plugin using custom logic
+     * - Return logger plugin instance
+     *
+     * @param \Everon\Logger\Configurator\AbstractPluginConfigurator $configurator
+     *
+     * @return \Everon\Logger\Contract\Plugin\LoggerPluginInterface
+     */
     public function create(AbstractPluginConfigurator $configurator): LoggerPluginInterface;
 }
